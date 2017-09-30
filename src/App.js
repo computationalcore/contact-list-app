a6043805ea8dc01974dc37079506c3d73135dd4d
Vin Busquet <computationalcore@gmail.com>
2017-09-29 22:59:56 -0300

    Save new contact to the server

--- src/App.js
+++ src/App.js
@@ -30,6 +30,14 @@ class App extends Component {
         ContactsAPI.remove(contact);
     };
 
+    createContact(contact) {
+        ContactsAPI.create(contact).then(contact => {
+            this.setState(state => ({
+                contacts: state.contacts.concat([ contact ])
+            }))
+        })
+    };
+
     render() {
         return (
             <div>
@@ -39,7 +47,14 @@ class App extends Component {
                         contacts={this.state.contacts}
                     />
                 )}/>
-                <Route path='/create' component={CreateContact}/>
+                <Route path='/create' render={({ history }) => (
+                    <CreateContact
+                        onCreateContact={(contact) => {
+                            this.createContact(contact)
+                            history.push('/')
+                        }}
+                    />
+                )}/>
             </div>
         )
     }
